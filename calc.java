e56c0684742471e296bc63ca94882adb7af44a64
Trung Nguyen <sendtotrungnguyen@outlook.com>
2023-04-28 12:02:27 +0300

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,9 +1,27 @@
-public class Calc
-{ 
- public int add(int a, int b){
- return a+b;
- } 
- public int sub(int a, int b){
- return a-b;
- }
+package Calculator;
+
+public class Calc {
+	public  int add(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public  int sub(int a, int b)
+	{
+		return a-b;
+	}
+	
+	 public int calculateRectangleArea(int a, int b) {
+	        if (a <= 0 || b <= 0) {
+	            throw new IllegalArgumentException("Error: height and width must be positive");
+	        }
+	        return a * b;
+	 }
+	 
+	 public double calculateCircleCircumference(double diameter) {
+	        if (diameter <= 0) {
+	            return 0;
+	        }
+	        return Math.PI * diameter;
+	    }
 }
